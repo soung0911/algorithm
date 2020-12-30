b56854036a481cf3f66d545c49d9c00b4878968b
Petros_K <soung0911@gmail.com>
2020-12-30 21:59:33 +0900

    Updates

--- sitemap-routes.js
+++ sitemap-routes.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 export default () => (
   <Switch>
     <Route path="/" exact  />
     <Route path="/site/:title"/>
     <Route path="/problem/:path/:title" />
-    <Redirect from="*" to="/" />
   </Switch>
 );
